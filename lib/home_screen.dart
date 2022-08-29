d48010a8e5b582dbd8bb5e68ad19d42610059917
SISU <atgsisu@gmail.com>
2022-08-30 08:28:22 +0900

    Create Sun and Moon

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,10 +15,35 @@ class _HomeScreenState extends State<HomeScreen> {
 
   var isPressed = false;
   var isDark = false;
+
+  Widget dayNight() {
+    return Container(
+      width: 210,
+      height: 210,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: isDark
+                  ? [Color(0xFF30218F), Color(0xFF8D81DD)]
+                  : [Color(0xFFFFCC81), Color(0xFFFF6E30)])),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: isDark ? primaryColorDark : primaryColorLight,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              dayNight(),
+            ]),
+      ),
     );
   }
 }
