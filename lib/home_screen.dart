122f856575ab34c13f44a833cbe12e67d6e74dbe
SISU <atgsisu@gmail.com>
2022-08-30 08:18:38 +0900

    Create new repo & Setting for new project

---
+++ lib/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final primaryColorLight = const Color(0xFFD8E0ED);
+  final primaryColorDark = const Color(0xFF2E3243);
+
+  var isPressed = false;
+  var isDark = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: isDark ? primaryColorDark : primaryColorLight,
+    );
+  }
+}
