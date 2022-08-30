610194516ef2f8d49dc47e2250f81262a64ba20a
SISU <atgsisu@gmail.com>
2022-08-30 11:11:27 +0900

    Finish up

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart' hide BoxShadow, BoxDecoration;
+import 'package:flutter/services.dart';
+
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -17,17 +18,84 @@ class _HomeScreenState extends State<HomeScreen> {
   var isDark = false;
 
   Widget dayNight() {
-    return Container(
-      width: 210,
-      height: 210,
-      decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: isDark
-                  ? [Color(0xFF30218F), Color(0xFF8D81DD)]
-                  : [Color(0xFFFFCC81), Color(0xFFFF6E30)])),
+    final positionShadow = isDark ? -40.0 : -200.0;
+    return Stack(
+      children: [
+        Container(
+          width: 210,
+          height: 210,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: isDark
+                      ? [Color(0xFF30218F), Color(0xFF8D81DD)]
+                      : [Color(0xFFFFCC81), Color(0xFFFF6E30)])),
+        ),
+        AnimatedPositioned(
+          duration: Duration(milliseconds: 200),
+          top: positionShadow,
+          right: positionShadow,
+          child: Container(
+            width: 210,
+            height: 210,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: isDark ? primaryColorDark : primaryColorLight),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget centerText() {
+    return Text(isDark ? 'Good\nNight' : 'Good\nMorning',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: isDark ? primaryColorLight : primaryColorDark));
+  }
+
+  Widget powerButton() {
+    return Listener(
+      onPointerDown: (_) => setState(() {
+        isPressed = true;
+      }),
+      onPointerUp: (_) => setState(() {
+        isPressed = false;
+        isDark = !isDark;
+        SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+            statusBarBrightness: isDark ? Brightness.dark : Brightness.light));
+      }),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 100),
+        width: 140,
+        height: 140,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: isDark ? primaryColorDark : primaryColorLight,
+            boxShadow: [
+              BoxShadow(
+                offset: const Offset(-5, 5),
+                blurRadius: 6,
+                // spreadRadius: 0.0,
+                color:
+                    isDark ? const Color(0xFF121625) : const Color(0xFFA5B7D6),
+                inset: isPressed,
+              ),
+              BoxShadow(
+                offset: const Offset(5, -5),
+                blurRadius: 6,
+                color: isDark ? const Color(0x4D9DA7CF) : Colors.white70,
+                inset: isPressed,
+              )
+            ]),
+        child: Icon(Icons.power_settings_new,
+            size: 48, color: isDark ? primaryColorLight : primaryColorDark),
+      ),
     );
   }
 
@@ -42,6 +110,10 @@ class _HomeScreenState extends State<HomeScreen> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               dayNight(),
+              const SizedBox(height: 36),
+              centerText(),
+              const SizedBox(height: 120),
+              powerButton(),
             ]),
       ),
     );
